51ee68d04f0ea1239e7a53a57b84e637db290c3a
h_h <hiromitsu@h-hnonotobukkukonpyuta.local>
2024-03-10 17:22:43 +0900

    記事一覧・詳細を取得するアクションの実装

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
+  # concern モジュールを読み込みMIみ
+  include Pagination
+
+  def index
+    # includesは関連先のテーブルのレコードをキャッシュとして取得するメソッド
+    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10).includes(:user)
+    render json: articles, meta: pagination(articles), adapter: :json
+  end
+
   def show
     # Article.publishedで公開中の記事から検索
     article = Article.published.find(params[:id])
