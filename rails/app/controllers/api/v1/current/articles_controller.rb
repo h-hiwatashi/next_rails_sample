4ee731af556ad2517727615fe5f8a50690d99dbd
h_h <hiromitsu@h-hnonotobukkukonpyuta.local>
2024-03-26 22:56:06 +0900

    feat: マイ記事の取得

--- rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails/app/controllers/api/v1/current/articles_controller.rb
@@ -4,7 +4,14 @@ class Api::V1::Current::ArticlesController < Api::V1::BaseController
   # before_actionでアクション全体に定義
   before_action :authenticate_user!
 
+  def index
+    # 未保存ステータスを除いたマイ記事を取得
+    articles = current_user.articles.not_unsaved.order(created_at: :desc)
+    render json: articles
+  end
+
   def create
+    # 未保存ステータスがない場合は新規作成
     unsaved_article = current_user.articles.unsaved.first || current_user.articles.create!(status: :unsaved)
     render json: unsaved_article
   end
